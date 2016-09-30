e7acf6d92d63cc3812ef0e7edcf76ce9d35c7361
Michael Cruz <michaelcruz@aim.com>
2016-09-30 16:39:20 -0400

    map setup, zoom into region center then stop updates

--- PokemonAdam/ViewController.swift
+++ PokemonAdam/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var updateCount = 0
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("We are ready to go!")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000,1000)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        }
     }
-
-
+    
+    
 }
-
